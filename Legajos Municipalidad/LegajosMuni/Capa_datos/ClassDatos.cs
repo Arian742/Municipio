fdf8ff57ba423ee0158062389aa55169844a7d86
Arian742 <138221522+Arian742@users.noreply.github.com>
2023-07-10 08:27:19 -0300

    Add files via upload

---
+++ Legajos Municipalidad/LegajosMuni/Capa_datos/ClassDatos.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using capa_entidad;
+
+namespace Capa_Datos
+{
+    public class ClassDatos
+    {
+        SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["sql"].ConnectionString);
+
+        public DataTable D_listar_agente()
+        {
+            SqlCommand cmd = new SqlCommand("SP_LISTAR_AGENTE", cn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+
+        public DataTable D_buscar_agente(ClassEntidad obje)
+        {
+            SqlCommand cmd = new SqlCommand("SP_BUSCAR_AGENTE", cn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@nombre", obje.nombre);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+
+        public String D_mantenimiento_agentes(ClassEntidad obje)
+        {
+            String accion = "";
+            SqlCommand cmd = new SqlCommand("SP_MANTENIMIENTO_AGENTE", cn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@codigo", obje.codigo);
+            cmd.Parameters.AddWithValue("@nombre", obje.nombre);
+            cmd.Parameters.AddWithValue("@edad", obje.edad);
+            cmd.Parameters.AddWithValue("@telefono", obje.telefono);
+            cmd.Parameters.Add("@accion", SqlDbType.VarChar, 50).Value = obje.accion;
+            cmd.Parameters["@accion"].Direction = ParameterDirection.InputOutput;
+            if (cn.State == ConnectionState.Open) cn.Close();
+            cn.Open();
+            cmd.ExecuteNonQuery();
+            accion = cmd.Parameters["@accion"].Value.ToString();
+            cn.Close();
+            return accion;
+
+        }
+
+    }
+}
